b0ce7300baf7010c0c4436c0fd68716b8ee9d693
4400113702 <oscarrcollazo@gmail.com>
2023-11-21 18:29:54 -0700

    V1.2

---
+++ CapstoneProject/AddTaskForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace CapstoneProject
+{
+    partial class AddTaskForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dueDateCalendar = new System.Windows.Forms.MonthCalendar();
+            this.taskNameLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.selectDateLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dueDateCalendar
+            // 
+            this.dueDateCalendar.Location = new System.Drawing.Point(93, 133);
+            this.dueDateCalendar.Name = "dueDateCalendar";
+            this.dueDateCalendar.TabIndex = 0;
+            // 
+            // taskNameLabel
+            // 
+            this.taskNameLabel.AutoSize = true;
+            this.taskNameLabel.Location = new System.Drawing.Point(20, 15);
+            this.taskNameLabel.Name = "taskNameLabel";
+            this.taskNameLabel.Size = new System.Drawing.Size(64, 15);
+            this.taskNameLabel.TabIndex = 1;
+            this.taskNameLabel.Text = "Task Name";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(20, 48);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(67, 15);
+            this.descriptionLabel.TabIndex = 2;
+            this.descriptionLabel.Text = "Description";
+            // 
+            // selectDateLabel
+            // 
+            this.selectDateLabel.AutoSize = true;
+            this.selectDateLabel.Location = new System.Drawing.Point(20, 133);
+            this.selectDateLabel.Name = "selectDateLabel";
+            this.selectDateLabel.Size = new System.Drawing.Size(55, 30);
+            this.selectDateLabel.TabIndex = 3;
+            this.selectDateLabel.Text = "Select\r\nDue Date";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 23);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 48);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(227, 73);
+            this.textBox2.TabIndex = 5;
+            // 
+            // AddTaskForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 313);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.selectDateLabel);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.taskNameLabel);
+            this.Controls.Add(this.dueDateCalendar);
+            this.Name = "AddTaskForm";
+            this.Text = "AddTaskForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MonthCalendar dueDateCalendar;
+        private Label taskNameLabel;
+        private Label descriptionLabel;
+        private Label selectDateLabel;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
---
+++ CapstoneProject/ViewAllForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CapstoneProject
+{
+    partial class ViewAllForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.viewAllListBox = new System.Windows.Forms.ListBox();
+            this.addTaskButton2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // viewAllListBox
+            // 
+            this.viewAllListBox.FormattingEnabled = true;
+            this.viewAllListBox.ItemHeight = 15;
+            this.viewAllListBox.Location = new System.Drawing.Point(29, 22);
+            this.viewAllListBox.Name = "viewAllListBox";
+            this.viewAllListBox.ScrollAlwaysVisible = true;
+            this.viewAllListBox.Size = new System.Drawing.Size(397, 439);
+            this.viewAllListBox.TabIndex = 0;
+            // 
+            // addTaskButton2
+            // 
+            this.addTaskButton2.Location = new System.Drawing.Point(351, 467);
+            this.addTaskButton2.Name = "addTaskButton2";
+            this.addTaskButton2.Size = new System.Drawing.Size(75, 23);
+            this.addTaskButton2.TabIndex = 1;
+            this.addTaskButton2.Text = "Add Task";
+            this.addTaskButton2.UseVisualStyleBackColor = true;
+            // 
+            // ViewAllForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 507);
+            this.Controls.Add(this.addTaskButton2);
+            this.Controls.Add(this.viewAllListBox);
+            this.Name = "ViewAllForm";
+            this.Text = "ViewAllForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox viewAllListBox;
+        private Button addTaskButton2;
+    }
+}
